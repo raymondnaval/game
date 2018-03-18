a6a96013793d69077f72c492702d79ec2713c9b6
raymondnaval <raymondnaval@192.168.0.21>
2018-03-17 20:32:26 -0700

    User sprite moves left and right.

--- core/src/com/raymondn/game/sprites/UserSprite.java
+++ core/src/com/raymondn/game/sprites/UserSprite.java
@@ -7,6 +7,7 @@ package com.raymondn.game.sprites;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
@@ -21,7 +22,7 @@ public class UserSprite {
     private Animation userAnimation;
     private Texture texture;
     private boolean enemyColliding = false, staticObjectColliding = false;
-    public static final int MOVE = 10;
+    public static final int MOVE = 1;
 
     public UserSprite(String userSprite, int x, int y) {
         position = new Vector2(x, y);
@@ -29,33 +30,40 @@ public class UserSprite {
         // userAnimation = new Animation(new TextureRegion(texture), 3, 0.5f);
         bounds = new Rectangle(x, y, texture.getWidth(), texture.getHeight());
         velocity = new Vector2();
+        
     }
 
-    // Change to updateX and create updateY.
-    public void update(float deltaTime) {
+    public void updateX(float deltaTime, boolean moveRight) {
 //        userAnimation.update(deltaTime);
         velocity.scl(deltaTime);
-        position.x += MOVE;
-        position.add(position.x, position.y);
-        System.out.println("move sprite");
+        if (moveRight) {
+            position.x += MOVE;
+        } else {
+            position.x -= MOVE;
+        }
+        position.set(position.x, position.y);
+        System.out.println("move sprite: " + position.x + " " + position.y);
     }
-    
+
+    public void updateY(float deltaTime) {
+    }
+
     public void updateBounds() {
         bounds.setPosition(position.x, position.y);
     }
-    
+
     public float getX() {
         return position.x;
     }
-    
+
     public float getY() {
         return position.y;
     }
-    
+
     public Texture getTexture() {
         return texture;
     }
-    
+
     public void dispose() {
         texture.dispose();
     }
--- core/src/com/raymondn/game/states/PlayState.java
+++ core/src/com/raymondn/game/states/PlayState.java
@@ -1,5 +1,6 @@
 package com.raymondn.game.states;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Texture;
@@ -15,7 +16,7 @@ public class PlayState extends State {
 
     private UserSprite user;
     private Texture bg;
-    private InputProcessor input;
+    private InputProcessor inputP;
 
     public PlayState(GameStateManager gsm) {
         super(gsm);
@@ -26,57 +27,19 @@ public class PlayState extends State {
 
     @Override
     protected void handleInput(final float deltaTime) {
-        input = new InputProcessor() {
-            @Override
-            public boolean keyDown(int keycode) {
-                if (keycode == Input.Keys.RIGHT) {
-                    user.update(deltaTime);
-                    System.out.println("keydown");
-                }
-                return true;
-            }
 
-            @Override
-            public boolean keyUp(int keycode) {
-                return false;
-            }
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            user.updateX(deltaTime, true);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            user.updateX(deltaTime, false);
+        }
 
-            @Override
-            public boolean keyTyped(char character) {
-                return false;
-            }
-
-            @Override
-            public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-                return false;
-            }
-
-            @Override
-            public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-                return false;
-            }
-
-            @Override
-            public boolean touchDragged(int screenX, int screenY, int pointer) {
-                return false;
-            }
-
-            @Override
-            public boolean mouseMoved(int screenX, int screenY) {
-                return false;
-            }
-
-            @Override
-            public boolean scrolled(int amount) {
-                return false;
-            }
-        };
     }
 
     @Override
     public void update(float dt) {
         handleInput(dt);
-        cam.position.x = user.getX() + UserSprite.MOVE;
         cam.update();
     }
 
@@ -84,7 +47,7 @@ public class PlayState extends State {
     public void render(SpriteBatch sb) {
         sb.setProjectionMatrix(cam.combined);
         sb.begin();
-        sb.draw(bg, cam.position.x - (cam.viewportWidth /2), 0);
+        sb.draw(bg, cam.position.x - (cam.viewportWidth / 2), 0);
         sb.draw(user.getTexture(), user.getX(), user.getY());
         sb.end();
     }
